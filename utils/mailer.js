68ee32154ae2ab357b57e20290ecbdb5401558eb
PardoCardona <rpardocardona@gmail.com>
2026-02-20 21:39:03 -0300

    Instalar Resend

--- utils/mailer.js
+++ utils/mailer.js
@@ -23,25 +23,3 @@ async function sendResetEmail(to, resetUrl) {
 }
 
 module.exports = { sendResetEmail };
-async function sendResetEmail(to, resetUrl) {
-  const transporter = createTransporter();
-
-  const mailOptions = {
-    from: `"GetLa" <${process.env.SMTP_USER}>`,
-    to,
-    subject: "Recuperación de contraseña - GetLa",
-    html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.5">
-        <h2>Recuperación de contraseña</h2>
-        <p>Recibimos una solicitud para restablecer tu contraseña.</p>
-        <p>Haz clic en el siguiente enlace (válido por 1 hora):</p>
-        <p><a href="${resetUrl}">${resetUrl}</a></p>
-        <p>Si tú no solicitaste esto, ignora este correo.</p>
-      </div>
-    `,
-  };
-
-  await transporter.sendMail(mailOptions);
-}
-
-module.exports = { sendResetEmail };

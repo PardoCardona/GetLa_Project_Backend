4f285873ed6a83228a55701490d0235f87c3e282
PardoCardona <rpardocardona@gmail.com>
2026-04-08 21:48:08 -0400

    Actualizacion modelo flota

---
+++ models/Flota/busModel.js
@@ -0,0 +1,45 @@
+//const models = require (../..Flota/busModel.js)
+const mongoose = require("mongoose");
+
+const busSchema = new mongoose.Schema(
+  {
+    imagen: {
+      type: String,
+      required: true,
+      trim: true,
+      
+    },
+
+    numeroInterno: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    placa: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+    },
+    marca: String,
+    modelo: String,
+    anio: Number,
+
+    estado: {
+      type: String,
+      enum: ["activo", "en_taller"],
+      default: "activo",
+    },
+
+    
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Bus", busSchema);
